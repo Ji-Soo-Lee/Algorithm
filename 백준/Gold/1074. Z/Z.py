d83d4fd2abfc5079dd4d1ffd79b46f256b1bc273
Ji Soo Lee <80959112+Ji-Soo-Lee@users.noreply.github.com>
2025-01-17 17:04:56 +0900

    [Gold V] Title: Z, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,61 +1,17 @@
-#!/usr/bin/env python3
-
 import sys
-from collections import deque
-
-def find_z(num, r, c, position, i):
-
-    if num == 0:
-        return position
-    
-    else:
-        if (r <= num//2) and (c <= num//2):
-            pos = 0
-        elif (r > num//2) and (c <= num//2):
-            pos = 2
-        elif (r <= num//2) and (c > num//2):
-            pos = 1
-        elif (r > num//2) and (c > num//2):
-            pos = 3
-
-        num = num//2
-
-        if pos == 3:
-            r -= 2**i
-            c -= 2**i
-        elif pos == 2:
-            r -= 2**i
-        elif pos == 1:
-            c -= 2**i
-        
-        if r < 0:
-            r = 0
-        if c < 0:
-            c = 0
-
-        i -= 1
-
-        position.append(pos)
-
-        find_z(num, r, c, position, i)
-
-        return position
 
 n, r, c = map(int, sys.stdin.readline().rstrip().split(' '))
 
-num = 2**n - 1
+x = n
+result = 0
+cnt = 0
 
-i = n - 1
+while x > 1:
+    div = 2**(x - 1)
+    result += 2**(2*(n - 1 - cnt)) * (2*(r//div) + c//div)
+    
+    x -= 1
+    r, c = r%div, c%div
+    cnt += 1
 
-position = deque()
-
-position = find_z(num, r, c, position, i)
-
-result = position.pop()
-
-for i in range(1, n):
-    num = position.pop()
-
-    result += 2**(2*i)*num
-
-print(result)
+print(result + 2*r + c)
